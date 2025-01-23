7b323426dc654bd3545f22f21f8e5228ec7f102a
Mahmoud <lamm.mah2002@gmail.com>
2025-01-23 22:59:20 +0100

    adding search bar to home component

--- src/new1/home.jsx
+++ src/new1/home.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 import Affichage from "./affichage"
 export default function Home(props){
+      const [serch , setsearch]=useState('')
+      const filtereddata =props.yy.filter((p)=>{
+        return p.title.toLowerCase().includes(serch.toLocaleLowerCase()
+         )
+      })
+
     return(
       //this component is where the all the data is selected
+
       <div className="row">
-        { props.yy.map((p)=>{
+           <div className="search-bar">
+        <input type="text" placeholder="Search..." value={serch} onChange={(e) => setsearch(e.target.value)} className="form-control mb-3"
+        />
+      </div>
+        { filtereddata.map((p)=>{
            return(
             
             <Affichage data={p}/>
@@ -12,3 +24,4 @@ export default function Home(props){
        </div>
     )
 }
+
